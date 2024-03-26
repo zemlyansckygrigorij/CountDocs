eca527254955e21e6915733e2afd77ef54f9904f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-03-26 15:38:17 +0500

    Initial commit

---
+++ src/test/java/com/example/countdocs/CountDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
