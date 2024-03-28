0e0ff23fb53321c28b9a690b90c14989b2f91200
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-03-28 14:48:22 +0500

    change class CountDocsApplication for run application

--- src/main/java/com/example/countdocs/CountDocsApplication.java
+++ src/main/java/com/example/countdocs/CountDocsApplication.java
@@ -1,13 +1,31 @@
 package com.example.countdocs;
 
+import com.example.countdocs.service.HandlerRootFolder;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class CountDocsApplication for run application
+ * */
 @SpringBootApplication
 public class CountDocsApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CountDocsApplication.class, args);
+    private static HandlerRootFolder handlerStatic;
+    @Autowired
+    HandlerRootFolder handler;
+    @PostConstruct
+    public void init() {
+        CountDocsApplication.handlerStatic = handler;
     }
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(CountDocsApplication.class, args);
 
+        if(args.length>0){
+            System.out.println(handlerStatic.getInfoAboutFilesByPath(args[0]));
+        }
+    }
 }
