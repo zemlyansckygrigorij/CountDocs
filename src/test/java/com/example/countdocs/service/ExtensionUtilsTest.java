d8a506e47bc0c36a2fe2dfd74e22f4d20be42693
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-03-28 14:45:57 +0500

    create class ExtensionUtilsTest for testing class ExtensionUtils

---
+++ src/test/java/com/example/countdocs/service/ExtensionUtilsTest.java
@@ -0,0 +1,47 @@
+package com.example.countdocs.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class ExtensionUtilsTest for testing class ExtensionUtils
+ * */
+@SpringBootTest
+class ExtensionUtilsTest {
+    @Autowired
+    ExtensionUtils utils;
+
+    @Test
+    @DisplayName(" Проверка получения количества страниц из pdf документа.")
+    void getCountPagesFromPDF(@Value("${test.path.pdf}") String path, @Value("${pages.count.pdf}") int countDocs) throws IOException {
+       int pagesCount =  getCountPagesFromDocument(new File(path));
+        assertEquals(countDocs,pagesCount);
+    }
+
+    @Test
+    @DisplayName(" Проверка получения количества страниц из docx документа.")
+    void getCountPagesFromDOCX(@Value("${test.path.docx}") String path, @Value("${pages.count.docx}") int countDocs) throws IOException {
+        int pagesCount =  getCountPagesFromDocument(new File("./src/test/resources/testData/1.docx"));
+        assertEquals(countDocs,pagesCount);
+    }
+
+    @Test
+    @DisplayName(" Проверка получения количества страниц из txt документа.")
+    void getCountPagesFromTXT(@Value("${test.path.txt}") String path, @Value("${pages.count.txt}") int countDocs) throws IOException {
+        int pagesCount =  getCountPagesFromDocument(new File("./src/test/resources/testData/1.txt"));
+        assertEquals(countDocs,pagesCount);
+    }
+
+    int getCountPagesFromDocument(File file) throws IOException {
+        return utils.getCountPagesFromDocument(file);
+    }
+}
