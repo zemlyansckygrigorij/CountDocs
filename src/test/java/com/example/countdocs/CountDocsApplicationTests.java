612b2b31339a22168518c013313de68da7df7aa5
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-03-28 14:49:05 +0500

    change class CountDocsApplicationTests for test load context

--- src/test/java/com/example/countdocs/CountDocsApplicationTests.java
+++ src/test/java/com/example/countdocs/CountDocsApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.countdocs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class CountDocsApplicationTests for test load context
+ * */
 @SpringBootTest
 class CountDocsApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
